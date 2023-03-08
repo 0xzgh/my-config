d86497fe9ba9492cb8d55792fbcbff8a585a472f
Roy <57223280+hoayo@users.noreply.github.com>
2023-03-08 12:02:42 +0800

    Update Microsoft.PowerShell_profile.ps1

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,4 +1,4 @@
-
+# Alias
 Set-Alias notepad D:\Notepad2_zh-Hans_x64_v4.22.01r4056\Notepad2.exe
 Set-Alias nc E:\Download\netcat-win32-1.12\nc64.exe
 Set-Alias codex D:\Programs\VSCodeInsiders\bin\code-insiders.cmd
@@ -7,7 +7,7 @@ Set-Alias nvim "D:\Programs\Neovim\bin\nvim.exe"
 Set-Alias upx "D:\Programs\upx-3.96-win64\upx.exe"
 
 Function ipy {python -m IPython}
-# 查找端口进程
+# Get process by port
 Function port{
 	param(
 	  [Parameter()]
@@ -19,7 +19,7 @@ Function port{
 	Get-Process -Id (Get-NetTCPConnection -LocalPort $p).OwningProcess
 	
 }
-# 查找进程pid
+# Get process by PID
 Function task{
 	param(
 	  [Parameter()]
@@ -32,17 +32,17 @@ Function task{
 function gcl {
 	git clone --recursive $args
 }
-# 新建文件
+# New File
 function touch {New-Item "$args" -ItemType File}
-# 终端代理
+# Terminal proxy
 function setproxy {netsh winhttp set proxy "127.0.0.1:7890"}
-# 检查代理
+# Show proxy info
 function showproxy {netsh winhttp show proxy}
-# 编辑PowershellShell配置文件
+# Edit powershell profile
 function profile {
   notepad $profile
 }
-# 函数示例：温度转换
+# Function example：Temperature
 function 2C {
   param (
       [Parameter()]
@@ -53,7 +53,7 @@ function 2C {
 
   ($Temperature - 30) / 2
 }
-# 命令行查词
+# Lookup english word 
 function dict{
 	param (
       [Parameter()]
@@ -63,13 +63,31 @@ function dict{
       )
       python E:\vscode-python\mdict-query\cmd.py $word	
 }
+# Get a fact from api
 function fact {
   irm -Uri https://uselessfacts.jsph.pl/random.json?language=en | Select -ExpandProperty text
 }
-# 随机joke
+# random joke from api
 function joke {
   irm https://icanhazdadjoke.com/ -Headers @{accept = 'application/json'} | select -ExpandProperty joke
 }
+
+function lf(){
+	python e:/vscode-python/file-size/list-size.py
+}
+
+# 更改默认ls行为，网格展示文件
+Remove-Alias -Name ls
+function ls {
+	Get-ChildItem | Format-Wide
+}
+# Terminal Icons color
+function set-color(){
+	Add-TerminalIconsColorTheme -Path C:\Users\roy\.config\colorTheme.psd1
+	Set-TerminalIconsTheme -ColorTheme royops
+}
+
+
 
 # 快速跳转zoxide配置
 # For zoxide v0.8.0+
@@ -78,28 +96,38 @@ Invoke-Expression (& {
     (zoxide init --hook $hook powershell | Out-String)
 })
 
-# 更改默认ls行为，网格展示文件
-Remove-Alias -Name ls
-function ls {
-	Get-ChildItem | Format-Wide
-}
 
 # 启用starship
 Invoke-Expression (&starship init powershell)
 
-
-
-#oh-my-posh init pwsh | Invoke-Expression
-#& ([ScriptBlock]::Create((oh-my-posh init pwsh --config "$env:POSH_THEMES_PATH\aliens.omp.json" --print) -join "`n"))
-
-
 # 导入图标
 Import-Module -Name Terminal-Icons
+
 # 设置自动补全
 Import-Module PSReadLine
 Set-PSReadLineOption -PredictionSource History
-# 提示颜色
-Set-PSReadLineOption -Colors @{ InlinePrediction = '#757679'}
+Set-PSReadLineOption -HistoryNoDuplicates
+Set-PSReadlineOption -Color @{
+    "ContinuationPrompt" = "`e[90m"     # Bright black
+    "Emphasis" = '#14fc97'              # Yellow
+    "Error" = "`e[31m"                  # Red
+    "Selection" = "`e[100m"             # Bright black background
+    "Default" = "`e[37m"                # White
+    "Comment" = "`e[3;90m"              # Bright black italics
+    "Keyword" = "`e[31m"                # Red
+    "String" = "`e[33m"                 # Yellow
+    "Operator" = "`e[31m"               # Red
+    "Variable" = "`e[36m"               # Cyan
+    #"Command" = "`e[38;2;219;112;147m"            # Gneen
+    "Command" = "`e[38;2;219;112;147;1;3m"            # Gneen
+    #"Command" = "e[1m"            # Gneen
+    #"Parameter" = "`e[34;4m"              # Red
+    "Parameter" = "`e[38;2;135;236;175;4m"             # Red
+    "Type" = "`e[36m"                   # Cyan
+    "Number" = "`e[35m"                 # Magenta
+    "Member" = "`e[37m"                 # White
+    "InlinePrediction" = '#757679'       # Bright black
+}
 
 # This custom binding makes `RightArrow` behave similarly - accepting the next word instead of the entire suggestion text.
 Set-PSReadLineKeyHandler -Key RightArrow `
@@ -118,6 +146,7 @@ Set-PSReadLineKeyHandler -Key RightArrow `
         [Microsoft.PowerShell.PSConsoleReadLine]::AcceptNextSuggestionWord($key, $arg)
     }
 }
+
 
 # This is an example of a macro that you might use to execute a command.
 # This will add the command to history.
@@ -160,26 +189,6 @@ Set-PSReadLineKeyHandler -Key Ctrl+LeftArrow `
     #[Microsoft.PowerShell.PSConsoleReadLine]::GetBufferState([ref]$line, [ref]$cursor)
     [Microsoft.PowerShell.PSConsoleReadLine]::RevertLine()
 }
-#Set-PSReadlineOption -Color @{
-#    "ContinuationPrompt" = "`e[90m"     # Bright black
-#    "Emphasis" = "`e[33m"               # Yellow
-#    "Error" = "`e[31m"                  # Red
-#    "Selection" = "`e[100m"             # Bright black background
-#    "Default" = "`e[37m"                # White
-#    "Comment" = "`e[3;90m"              # Bright black italics
-#    "Keyword" = "`e[31m"                # Red
-#    "String" = "`e[33m"                 # Yellow
-#    "Operator" = "`e[31m"               # Red
-#    "Variable" = "`e[36m"               # Cyan
-#    "Command" = "`e[32m"                # Gneen
-#    "Parameter" = "`e[34m"              # Red
-#    "Type" = "`e[36m"                   # Cyan
-#    "Number" = "`e[35m"                 # Magenta
-#    "Member" = "`e[37m"                 # White
-#    "InlinePrediction" = "`e[90m"       # Bright black
-#}
-
-cls
 
 # Import the Chocolatey Profile that contains the necessary code to enable
 # tab-completions to function for `choco`.
@@ -190,3 +199,4 @@ $ChocolateyProfile = "$env:ChocolateyInstall\helpers\chocolateyProfile.psm1"
 if (Test-Path($ChocolateyProfile)) {
   Import-Module "$ChocolateyProfile"
 }
+cls
